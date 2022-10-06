54848da4e036f2dfa8205d4151f088238cfd08dd
Saeejith Nair <saeejithn@gmail.com>
2022-10-06 01:24:03 -0400

    Formatted convert2tensorrt.py

--- scripts/conversion/convert2tensorrt.py
+++ scripts/conversion/convert2tensorrt.py
@@ -14,6 +14,7 @@ from tensorrt_session import TRTSession
 ONNX_EXTENSION = 'onnx'
 TRT_EXTENSION = 'engine'
 
+
 def get_files_from_dir(dir, extension):
     files_list = glob.glob(f'{dir}/*.{extension}')
 
@@ -26,7 +27,7 @@ def convert_to_tensorrt(export_configs):
     try:
         onnx_file_path = f"{onnx_models_dir}/{onnx_filename}.{ONNX_EXTENSION}"
         trt_output_path = f"{trt_output_dir}/{onnx_filename}.{TRT_EXTENSION}"
-        
+
         trt_session = TRTSession()
         serialized_engine = trt_session.onnx_to_tensorrt(onnx_file_path)
         with open(trt_output_path, "wb") as f:
@@ -36,41 +37,53 @@ def convert_to_tensorrt(export_configs):
             print(f"Finished writing {trt_output_path} to disk.")
             sys.stdout.flush()
     except Exception as e:
-        print(f'ERROR. Encountered exception while trying to convert architecture {onnx_filename}')
+        print(f"ERROR. Encountered exception while trying to convert "
+              f"architecture {onnx_filename}")
         print(e)
         if os.path.exists(trt_output_path):
             os.remove(trt_output_path)
             print(f"Removed TRT file {trt_output_path}.")
 
-def filter_generated_models(onnx_models_dir, trt_output_dir):    
+
+def filter_generated_models(onnx_models_dir, trt_output_dir):
     onnx_models_dir = os.path.abspath(onnx_models_dir)
     trt_output_dir = os.path.abspath(trt_output_dir)
 
-    onnx_file_paths = get_files_from_dir(onnx_models_dir, extension=ONNX_EXTENSION)
-    trt_file_paths = get_files_from_dir(trt_output_dir, extension=TRT_EXTENSION)
+    onnx_file_paths = get_files_from_dir(onnx_models_dir,
+                                         extension=ONNX_EXTENSION)
+    trt_file_paths = get_files_from_dir(trt_output_dir,
+                                        extension=TRT_EXTENSION)
 
-    onnx_filenames = set([Path(file_path).stem for file_path in onnx_file_paths])
-    trt_filenames = set([Path(file_path).stem for file_path in trt_file_paths])
+    onnx_filenames = set([Path(file_path).stem
+                         for file_path in onnx_file_paths])
+    trt_filenames = set([Path(file_path).stem
+                        for file_path in trt_file_paths])
 
     remaining_onnx_filenames = list(onnx_filenames.difference(trt_filenames))
     remaining_onnx_filenames.sort()
-    print(f"Found {len(remaining_onnx_filenames)} models left to be converted out of total {len(onnx_file_paths)}.")
+    print(f"Found {len(remaining_onnx_filenames)} models left to be "
+          f"converted out of total {len(onnx_file_paths)}.")
 
     return remaining_onnx_filenames
 
-def convert_to_tensorrt_parallel(onnx_models_dir, trt_output_dir, num_files_to_convert):
-    remaining_onnx_filenames = filter_generated_models(onnx_models_dir, trt_output_dir)
-    num_files_to_convert = min(num_files_to_convert, len(remaining_onnx_filenames))
-    remaining_onnx_filenames = remaining_onnx_filenames[:num_files_to_convert]
-    print(f"Converting {len(remaining_onnx_filenames)} ONNX files.")
-    
-    export_configs = [(onnx_filename, onnx_models_dir, trt_output_dir) for onnx_filename in remaining_onnx_filenames]
-    pool = mp.Pool(mp.cpu_count())
-    pool.map(convert_to_tensorrt, export_configs)
 
-def convert_to_tensorrt_sequential(onnx_models_dir, trt_output_dir, num_files_to_convert):
-    remaining_onnx_filenames = filter_generated_models(onnx_models_dir, trt_output_dir)
-    num_files_to_convert = min(num_files_to_convert, len(remaining_onnx_filenames))
+def convert_to_tensorrt_sequential(onnx_models_dir, trt_output_dir,
+                                   num_files_to_convert):
+    """Converts models in directory from ONNX to TensorRT.
+
+    Args:
+        onnx_models_dir: Directory containing input ONNX model files.
+        trt_output_dir: Directory to output converted TensorRT models to.
+        num_files_to_convert: Helps control the maximum number of models to
+            convert in one go. Jetson devices have a weird issue where GPU
+            memory might not get released after the model is converted. Hence
+            it is best to run this script on just a few models at a time as
+            GPU memory will be released once this script terminates.
+    """
+    remaining_onnx_filenames = filter_generated_models(onnx_models_dir,
+                                                       trt_output_dir)
+    num_files_to_convert = min(num_files_to_convert,
+                               len(remaining_onnx_filenames))
     remaining_onnx_filenames = remaining_onnx_filenames[:num_files_to_convert]
     print(f"Converting {len(remaining_onnx_filenames)} ONNX files.")
 
@@ -78,22 +91,35 @@ def convert_to_tensorrt_sequential(onnx_models_dir, trt_output_dir, num_files_to
         export_configs = (onnx_filename, onnx_models_dir, trt_output_dir)
         convert_to_tensorrt(export_configs)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", default=27, dest="seed", help="Seed value for random generator.")
-    parser.add_argument("--export_dir", default="/home/saeejith/work/nas/hw-nats-bench/datasets", dest="export_dir", help="Output directory to store exported TensorRT files.")
-    parser.add_argument("--input_dir", default="/home/saeejith/work/nas/hw-nats-bench/datasets/onnx-simplified", dest="input_dir", help="Input directory containing ONNX models.")
-    parser.add_argument("--range", nargs=2, default=[0, 15625], dest="range", type=(int), help="Range of architectures to export")
-    parser.add_argument('--convert_parallel', dest='convert_parallel', action='store_true')
-    parser.add_argument('--num_models_to_convert', default=15625, dest='num_models_to_convert', type=(int), help="Number of models to convert")
+    parser.add_argument(
+        "--seed", default=27, dest="seed",
+        help="Seed value for random generator.")
+    parser.add_argument(
+        "--export_dir",
+        default="/home/saeejith/work/nas/hw-nats-bench/datasets",
+        dest="export_dir",
+        help="Output directory to store exported TensorRT files.")
+    parser.add_argument(
+        "--input_dir",
+        default="/home/saeejith/work/nas/hw-nats-bench/datasets/onnx-simplified",
+        dest="input_dir", help="Input directory containing ONNX models.")
+    parser.add_argument(
+        "--range", nargs=2, default=[0, 15625], dest="range", type=(int),
+        help="Range of architectures to export")
+    parser.add_argument(
+        "--num_models_to_convert", default=15625,
+        dest='num_models_to_convert', type=(int),
+        help="Number of models to convert")
 
     args = parser.parse_args()
     assert (len(args.range) == 2)
 
-    gen_utils.setup_seed(args.seed)
+    utils.setup_seed(args.seed)
 
-    # Execute conversion.
-    if args.convert_parallel:
-        convert_to_tensorrt_parallel(args.input_dir, args.export_dir, args.num_models_to_convert)
-    else:
-        convert_to_tensorrt_sequential(args.input_dir, args.export_dir, args.num_models_to_convert)
+    # Execute conversion sequentially (Jetson devices do not have)
+    # enough memory to support parallel conversions.
+    convert_to_tensorrt_sequential(args.input_dir, args.export_dir,
+                                   args.num_models_to_convert)
