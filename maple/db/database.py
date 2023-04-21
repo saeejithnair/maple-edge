dc9c1c587e36d1563f0e1daef9a17e6b76cba5ab
Saeejith Nair <saeejithn@gmail.com>
2023-04-21 19:52:01 -0400

    Improved type hinting for AsyncEngine

--- maple/db/database.py
+++ maple/db/database.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, create_engine
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlmodel.ext.asyncio.session import AsyncSession
 from contextlib import asynccontextmanager
 
@@ -17,22 +17,22 @@ def get_database_url(username: str = USERNAME, password: str = PASSWORD,
                      host: str = HOST, database: str = DATABASE) -> str:
     return f"postgresql+asyncpg://{username}:{password}@{host}/{database}"
 
-async def create_db_and_tables(engine: "AsyncEngine") -> None:
+async def create_db_and_tables(engine: AsyncEngine) -> None:
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
 
 @asynccontextmanager
-async def get_session(engine: "AsyncEngine") -> "AsyncSession":
+async def get_session(engine: AsyncEngine) -> AsyncSession:
     async with AsyncSession(engine) as session:
         yield session
 
-def create_engine(database_url: str) -> "AsyncEngine":
+def create_engine(database_url: str) -> AsyncEngine:
     return create_async_engine(
         database_url,
         future=True,
     )
 
-async def init_db(database: str = DATABASE) -> "AsyncEngine":
+async def init_db(database: str = DATABASE) -> AsyncEngine:
     database_url = get_database_url(database=database)
     async_engine = create_engine(database_url)
     await create_db_and_tables(engine=async_engine)
